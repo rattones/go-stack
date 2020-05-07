5f201a97284fb963212795a792affbbd0989e988
Marcelo Ratton <rattones@marcelorattoncom>
2020-05-07 17:00:11 -0300

    Criando a aplicação: Navegando entre as Rotas

--- github-explorer-react/src/pages/Dashboard/index.tsx
+++ github-explorer-react/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -71,7 +72,7 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href='#'>
+          <Link key={repository.full_name} to={`repositories/${repository.full_name}`}>
             <img src={repository.owner.avatar_url}
               alt={repository.owner.login}/>
 
@@ -81,8 +82,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
-
+          </Link>
         ))}
         </Repositories>
     </>
