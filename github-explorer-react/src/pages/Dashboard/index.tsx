326835a0cb5e84c4536656a528226e55cace307b
Marcelo Ratton <rattones@marcelorattoncom>
2020-05-07 16:37:09 -0300

    Criando a aplicação: Salvando no Storage

--- github-explorer-react/src/pages/Dashboard/index.tsx
+++ github-explorer-react/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
@@ -19,7 +19,15 @@ const Dashboard: React.FC = () => {
 
   const [ newRepo, setNewRepo ] = useState('');
   const [ inputError, setInputError ] = useState('');
-  const [ repositories, setRepositories ] = useState<Repository[]>([]);
+  const [ repositories, setRepositories ] = useState<Repository[]>(() => {
+    const storageRepositories = localStorage.getItem('@GithubExplorer:repositories');
+
+    return storageRepositories? JSON.parse(storageRepositories): [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories));
+  }, [repositories])
 
   async function handleSubmit(event: FormEvent<HTMLFormElement> ): Promise<void> {
     event.preventDefault();
